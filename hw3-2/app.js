21bb12db8f84c4e62dc44d852ea63367b96c4981
berbolot <berbolotd@gmail.com>
2023-01-27 15:39:53 +0500

    first commit

---
+++ hw3-2/app.js
@@ -0,0 +1,11 @@
+var number = 0;
+var numbers = 0;
+do{
+    number = numbers + number
+    numbers = Number(prompt('Введите число'));  
+    
+}while (numbers > 0);
+console.log('Положительные число' + number);
+
+
+
